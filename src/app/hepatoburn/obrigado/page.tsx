f4ab55a019c08cbbd4f5141c16811d0e004777ef
Bruno Galiz de Oliveira <brunogaliz@hotmail.com>
2025-07-25 16:14:26 -0400

    fix

--- src/app/hepatoburn/obrigado/page.tsx
+++ src/app/hepatoburn/obrigado/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import Image from "next/image";
 
 export default function ObrigadoPage() {
   const searchParams = useSearchParams();
